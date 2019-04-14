4a16df6b25a03602a78614673b69fe63850f2f5f
Raghuveer Singh <raghu1213@hotmail.com>
2019-04-14 18:24:23 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   console.log("request received");
-  setTimeout(function () {
+  await setTimeout(function () {
     console.log('Blah blah blah blah extra-blah');
-  },120000);
-  res.render('index', { title: 'Express' });
+  }, 120000).then(() => {
+    res.render('index', { title: 'Express' });
+  });
+  
 });
 
 module.exports = router;
